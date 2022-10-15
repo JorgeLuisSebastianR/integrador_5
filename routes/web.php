999d87ff564491d05c55ced87815b9574153371f
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-10-14 23:05:11 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonalController;
+use App\Http\Controllers\PuestoController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\PlantaController;
+use App\Http\Controllers\OrdenProduccionController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\MaterialOrdenProduccionController;
+use App\Http\Controllers\ColorController;
+use App\Http\Controllers\QuimicoColorController;
+use App\Http\Controllers\TratamientoController;
+use App\Http\Controllers\QuimicoTratamientoController;
+use App\Http\Controllers\QuimicoController;
 
 
 Route::get('/', function () {
