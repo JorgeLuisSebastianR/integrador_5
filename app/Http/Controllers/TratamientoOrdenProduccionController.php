cb21b1916fc79d9c0225493dced6c29e9644f74c
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-10-22 13:01:17 -0500

    .

---
+++ app/Http/Controllers/TratamientoOrdenProduccionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TratamientoOrdenProduccion;
+use App\Http\Requests\StoreTratamientoOrdenProduccionRequest;
+use App\Http\Requests\UpdateTratamientoOrdenProduccionRequest;
+
+class TratamientoOrdenProduccionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTratamientoOrdenProduccionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTratamientoOrdenProduccionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TratamientoOrdenProduccion  $tratamientoOrdenProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TratamientoOrdenProduccion $tratamientoOrdenProduccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TratamientoOrdenProduccion  $tratamientoOrdenProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TratamientoOrdenProduccion $tratamientoOrdenProduccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTratamientoOrdenProduccionRequest  $request
+     * @param  \App\Models\TratamientoOrdenProduccion  $tratamientoOrdenProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTratamientoOrdenProduccionRequest $request, TratamientoOrdenProduccion $tratamientoOrdenProduccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TratamientoOrdenProduccion  $tratamientoOrdenProduccion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TratamientoOrdenProduccion $tratamientoOrdenProduccion)
+    {
+        //
+    }
+}
