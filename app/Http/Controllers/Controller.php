66eb0d1b07e02a3b759e116f4bf36bb826ffd7b9
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-10-22 10:58:43 -0500

    .

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+
+//aaaaaaaaaagggg
+class Controller extends BaseController
+{//zaaaaa
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
