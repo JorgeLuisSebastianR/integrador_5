3bbb5323fc8e18accc8e79e18c600a4da111be88
Marco20TE0541 <rodriguezmendezmarcoantonioh@gmail.com>
2022-10-22 11:03:10 -0500

    Migracion Empleado

--- database/migrations/2022_10_22_155210_create_empleados_table.php
+++ database/migrations/2022_10_22_155210_create_empleados_table.php
@@ -14,8 +14,34 @@ return new class extends Migration
     public function up()
     {
         Schema::create('empleados', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+         //me falto poner completo el nombre del id
+         $table->id();
+         /*  //llaves foraneas
+          $table->unsignedBigInteger('idDepartamento');
+          $table->foreign('idDepartamento')->references('id')->on('departamento');
+
+          $table->unsignedBigInteger('idPuesto');
+          $table->foreign('idPuesto')->references('id')->on('puesto');*/
+
+          //atributos normales
+          $table->string('numTrabajador', 10);
+          $table->string('nombre', 60);
+          $table->string('apellidoPaterno', 50);
+          $table->string('apellidoMaterno', 50);
+          $table->string('domicilio', 100);
+          $table->string('RFC', 11);
+          $table->string('CURP', 18);
+          $table->string('NSS', 20);
+          $table->date('fechaIngreso');
+          $table->enum('sexo', ['H', 'M']);
+          $table->date('fechaNacimiento');
+          $table->enum('estadoSalud', ['Saludable', 'Discapacidad','Enfermedad']);
+          $table->string('observaciones', 1000);
+          $table->enum('estadoCivil', ['Casado', 'Soltero','Viudo']);
+          $table->string('telefonoContacto', 10);
+          $table->string('correoElectronico', 50);
+          $table->enum('estatus', ['Activo', 'Renuncia','Baja','Despido']);
+          $table->timestamps();
         });
     }
 
